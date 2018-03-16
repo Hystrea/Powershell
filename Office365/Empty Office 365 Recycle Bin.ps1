e59650cfe5e576ac9aa0aea89445bf75051784e7
LNMaarten <maarten@limenetworks.nl>
2018-03-16 11:06:58 +0100

    Push script
    Push script initial

---
+++ Office365/Empty Office 365 Recycle Bin.ps1
@@ -0,0 +1,8 @@
+﻿### Logt in op O365 services ###
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+$CRED = Get-Credential
+$SESSION = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $CRED -Authentication Basic -AllowRedirection
+Import-PSSession $SESSION
+Connect-MsolService -Credential $CRED
+# Verwijdert alle users in de O365 recycle bin.
+Get-MsolUser -ReturnDeletedUsers | Remove-MsolUser -RemoveFromRecycleBin -Force
