7eb369b740262edd2b609e0a83521524dffd0331
AadSchimmel <aad@limenetworks.nl>
2018-03-16 11:19:37 +0100

    Initial
    First commit

---
+++ Windows/DSC-Services.ps1
@@ -0,0 +1,23 @@
+﻿$GetService = Get-Service | select DisplayName,Name,StartType
+$CSV = @"
+DisplayName,Name,StartType
+DNS Server,DNS,Automatic
+DHCP Server,DHCP,Automatic
+Backup Service Controller,Backup Service Controller,Automatic
+Remote Desktop Gateway,TSGateway,Automatic
+"@
+
+$Base = ConvertFrom-Csv -InputObject $CSV
+
+ForEach ($Service in $GetService){
+ForEach ($State in $Base){
+ 
+if ($State.StartType -ne $Service.StartType -and $State.DisplayName -eq $Service.DisplayName){
+
+Write-Host $Service.DisplayName state is $Service.StartType and should be $State.StartType
+
+#Set-Service -DisplayName $Service.DisplayName -StartupType Automatic
+#Start-Service -DisplayName $Service.DisplayName -Force -verbose
+
+ 
+}}}
