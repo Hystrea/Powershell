9ced423eaeb5907790ca4b1f5b2e003af87efa83
LNMaarten <maarten@limenetworks.nl>
2018-03-26 09:48:27 +0200

    Push initial
    Script push

---
+++ Windows/Speech-selection.ps1
@@ -0,0 +1,20 @@
+﻿#requires -Version 2.0
+Add-Type -AssemblyName System.Speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$ssml = '
+<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" 
+    xml:lang="en-US">
+    <voice xml:lang="en-US">
+    <prosody rate="1">
+        <p>Holy shit</p>
+    </prosody>
+    </voice>
+    <voice xml:lang="en-US">
+    <prosody rate="0">
+        <p>...you make me so tired.</p>
+    </prosody>
+    </voice>
+</speak>
+'
+
+$speak.SpeakSsml($ssml)
